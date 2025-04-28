a29e561fa8bf013759d7ec827491b3c94a54134c
Redger Xu <redgerxu@gmail.com>
2025-04-28 12:04:41 -0700

    answer checking with cortex

--- server/routes/api/problem.ts
+++ server/routes/api/problem.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import { Problem } from "@server/schemas";
 import { ComputeEngine } from "@cortex-js/compute-engine";
+import { log } from "@common/util";
 
 export const problemRouter = express.Router();
 
 const ce = new ComputeEngine();
 
 function compute(expr: string): number {
-  return ce.parse(expr).evaluate().value as number;
+  return ce.parse(expr).N().value as number;
+}
+
+function approx(a: number, b: number) {
+  return Math.abs(a - b) <= 1e-10;
 }
 
 problemRouter.post("/:id/verify", async (req, res) => {
@@ -21,9 +26,21 @@ problemRouter.post("/:id/verify", async (req, res) => {
     const latex: string = (req.body.latex ?? "").replace(/ /g, "");
     const solution = problem.answer!.replace(/ /g, "");
 
+    if (req.user) {
+      log(
+        `${req.user.username} submitted answer ${latex} to problem ${id} (eval: ${compute(latex)})`,
+      );
+    } else {
+      log(`Answer ${latex} submitted to problem ${id} (eval: ${compute(latex)})`);
+    }
+
+    const result = approx(compute(latex), compute(solution));
+
+    log(`Correct: ${result}, answer was ${compute(solution)}`);
+
     // compute working
     // TODO: rewrite all of the answers
 
-    res.send({ result: latex === solution });
+    res.send({ result });
   }
 });
